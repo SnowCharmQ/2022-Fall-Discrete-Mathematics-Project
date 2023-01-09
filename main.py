f9d666094ea48705bef9edfe456e48bf9d00fd1b
SnowCharm <619022098@qq.com>
2023-01-09 13:58:18 +0800

    add RSA implementation

---
+++ main.py
@@ -0,0 +1,84 @@
+import random
+
+
+def gcd(a, b):
+    # Euclidean algorithm for finding GCD
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def find_mod_inverse(a, m):
+    # Extended Euclidean algorithm for finding modular inverse
+    m0 = m
+    y = 0
+    x = 1
+
+    if m == 1:
+        return 0
+
+    while a > 1:
+        q = a // m
+        t = m
+        m = a % m
+        a = t
+        t = y
+        y = x - q * y
+        x = t
+
+    if x < 0:
+        x = x + m0
+
+    return x
+
+
+def generate_keypair(p, q):
+    # Generate public and private keys
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclidean algorithm to verify that e and phi(n) are coprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclidean algorithm to generate the private key
+    d = find_mod_inverse(e, phi)
+
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return (e, n), (d, n)
+
+
+def encrypt(pk, plaintext):
+    # Encrypt the plaintext using the public key
+    key, n = pk
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    # Decrypt the ciphertext using the private key
+    key, n = pk
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    return ''.join(plain)
+
+
+if __name__ == '__main__':
+    print("RSA Encryption and Decryption")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your public key: ")
+    encrypted_msg = encrypt(public, message)
+    print("Your encrypted message is: ")
+    print(bin(int(''.join(map(lambda x: str(x), encrypted_msg)))))
+    print("Decrypting message with private key . . .")
+    print("Your message is:")
+    print(decrypt(private, encrypted_msg))
